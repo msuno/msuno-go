9909bd6881e2a47a6f80b50a73605bfc8e569f3d
一指流沙 <1848030634@qq.com>
2019-05-31 00:22:08 +0800

    增加sqlite3

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/astaxie/beego"
+	_ "github.com/mattn/go-sqlite3"
 	_ "web/routers"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3","./data.db")
+	if err != nil {
+		panic(err.Error())
+	}
+	sql_table := `
+		CREATE TABLE IF NOT EXISTS "userinfo" (
+		   "uid" INTEGER PRIMARY KEY AUTOINCREMENT,
+		   "username" VARCHAR(64) NULL,
+		   "departname" VARCHAR(64) NULL,
+		   "created" TIMESTAMP default (datetime('now', 'localtime'))  
+		);`
+	db.Exec(sql_table)
 	beego.Run()
 }
